902864998a255a54b797091a1596522a7666e2ea
luis <luismanuelamengual@gmail.com>
2017-05-14 19:01:36 -0300

    Adding comments

--- src/main/java/org/neogroup/httpserver/HttpResponse.java
+++ src/main/java/org/neogroup/httpserver/HttpResponse.java
@@ -7,6 +7,9 @@ import java.nio.ByteBuffer;
 import java.text.MessageFormat;
 import java.util.*;
 
+/**
+ * This class holds information for a Http Response
+ */
 public class HttpResponse {
 
     private final static String STATUS_LINE_TEMPLATE = "HTTP/1.1 {0} {1}\r\n";
@@ -22,10 +25,17 @@ public class HttpResponse {
     private int bodySize;
     private boolean headersSent;
 
+    /**
+     * Default constructor for a http response
+     */
     public HttpResponse () {
         this(HttpConnection.getActiveConnection());
     }
 
+    /**
+     * Constructor for a response with thte associated connection
+     * @param connection Http connection
+     */
     public HttpResponse(HttpConnection connection) {
         this.connection = connection;
         this.headers = new HashMap<>();
@@ -35,18 +45,35 @@ public class HttpResponse {
         bodySize = 0;
     }
 
+    /**
+     * Obtains the response code of a response
+     * @return int response code
+     */
     public int getResponseCode() {
         return responseCode;
     }
 
+    /**
+     * Set response code of a response
+     * @param responseCode int response code
+     */
     public void setResponseCode(int responseCode) {
         this.responseCode = responseCode;
     }
 
+    /**
+     * Retrieve the headers of a response
+     * @return Headers of the response
+     */
     public Map<String,List<String>> getHeaders() {
         return Collections.unmodifiableMap(headers);
     }
 
+    /**
+     * Add a new header to the response
+     * @param headerName Header name
+     * @param headerValue Header value
+     */
     public void addHeader(String headerName, String headerValue) {
         List<String> headerValues = headers.get(headerName);
         if (headerValues == null) {
@@ -56,6 +83,11 @@ public class HttpResponse {
         headerValues.add(headerValue);
     }
 
+    /**
+     * Retrieve the value of a header
+     * @param headerName name of the header
+     * @return value of the header
+     */
     public String getHeader (String headerName) {
         String value = null;
         List<String> headerValues = headers.get(headerName);
@@ -65,22 +97,43 @@ public class HttpResponse {
         return value;
     }
 
+    /**
+     * Retrieve all the values for a given header
+     * @param headerName name of the header
+     * @return values for a header
+     */
     public List<String> getHeaders (String headerName) {
         return headers.get(headerName);
     }
 
+    /**
+     * Remove all headers with a given name
+     * @param headerName name of the header
+     */
     public void removeHeader (String headerName) {
         headers.remove(headerName);
     }
 
+    /**
+     * Indicates if the response contains a given header
+     * @param headerName name of the header
+     * @return boolean
+     */
     public boolean hasHeader (String headerName) {
         return headers.containsKey(headerName);
     }
 
+    /**
+     * Remove all headers
+     */
     public void clearHeaders () {
         headers.clear();
     }
 
+    /**
+     * Adds a new cookie to the response
+     * @param cookie cookie to add
+     */
     public void addCookie (HttpCookie cookie) {
         StringBuilder cookieValue = new StringBuilder();
         cookieValue.append(cookie.getName());
@@ -109,18 +162,34 @@ public class HttpResponse {
         addHeader(HttpHeader.SET_COOKIE, cookieValue.toString());
     }
 
+    /**
+     * Sets the content of the response
+     * @param body content of the response
+     */
     public void setBody(String body) {
         setBody(body.getBytes());
     }
 
+    /**
+     * Sets the content of the response
+     * @param body content of the response
+     */
     public void setBody(byte[] body) {
         write(body);
     }
 
+    /**
+     * Writes content in the response
+     * @param text text to write in the response
+     */
     public void write (String text) {
         write(text.getBytes());
     }
 
+    /**
+     * Write content in the response
+     * @param bytes bytes to write in the response
+     */
     public void write (byte[] bytes) {
 
         bodySize += bytes.length;
@@ -141,10 +210,16 @@ public class HttpResponse {
         }
     }
 
+    /**
+     * Flushes content in the response
+     */
     public void flush () {
         writeBuffer();
     }
 
+    /**
+     * Send headers with the response
+     */
     private void sendHeaders () {
         if (!headersSent) {
             addHeader(HttpHeader.DATE, HttpServerUtils.formatDate(new Date()));
@@ -196,6 +271,9 @@ public class HttpResponse {
         }
     }
 
+    /**
+     * Writes the buffered content
+     */
     protected void writeBuffer() {
 
         sendHeaders();
